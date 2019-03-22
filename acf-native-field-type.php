4f43851219d0f78cde941ff35b28924291d1b96f
Nestor Vera <nestorvera@me.com>
2019-03-22 20:18:20 +0100

    Check if plugin is active

--- acf-native-field-type.php
+++ acf-native-field-type.php
@@ -24,6 +24,11 @@ class acf_field_native extends acf_field {
 	}
 
 	function render_field_settings( $field ) {
+		$active_plugins = apply_filters( 'active_plugins', get_option( 'active_plugins' ) );
+		$is_active      = function ( $plugin ) use ( $active_plugins ) {
+			return in_array( $plugin, $active_plugins, true );
+		};
+
 		acf_render_field_setting(
 			$field,
 			array(
@@ -33,50 +38,52 @@ class acf_field_native extends acf_field {
 				'name'         => 'native_field',
 				'required'     => 1,
 				// TODO: Implement backend and frontend functionality for custom native fields (hooks)
-				'choices'      => array(
-					'Core'        => array(
-						'content'         => __( 'Content Editor', 'acf-native-fields' ),
-						'excerpt'         => __( 'Excerpt', 'acf-native-fields' ),
-						'publish_box'     => __( 'Publish Box', 'acf-native-fields' ),
-						'page_attributes' => __( 'Page Attributes', 'acf-native-fields' ),
-						'featured_image'  => __( 'Featured Image', 'acf-native-fields' ),
+				'choices'      => array_filter(
+					array(
+						'Core'        => array(
+							'content'         => __( 'Content Editor', 'acf-native-fields' ),
+							'excerpt'         => __( 'Excerpt', 'acf-native-fields' ),
+							'publish_box'     => __( 'Publish Box', 'acf-native-fields' ),
+							'page_attributes' => __( 'Page Attributes', 'acf-native-fields' ),
+							'featured_image'  => __( 'Featured Image', 'acf-native-fields' ),
 
-						'permalink'       => __( 'Permalink', 'acf-native-fields' ),
-						'author'          => __( 'Author', 'acf-native-fields' ),
+							'permalink'       => __( 'Permalink', 'acf-native-fields' ),
+							'author'          => __( 'Author', 'acf-native-fields' ),
 
-						'categories'      => __( 'Categories', 'acf-native-fields' ),
-						'tags'            => __( 'Tags', 'acf-native-fields' ),
-						'format'          => __( 'Format', 'acf-native-fields' ),
+							'categories'      => __( 'Categories', 'acf-native-fields' ),
+							'tags'            => __( 'Tags', 'acf-native-fields' ),
+							'format'          => __( 'Format', 'acf-native-fields' ),
 
-						'revisions'       => __( 'Revisions', 'acf-native-fields' ),
-						'comments'        => __( 'Comments', 'acf-native-fields' ),
-						'discussion'      => __( 'Discussion', 'acf-native-fields' ),
-						'trackbacks'      => __( 'Trackbacks', 'acf-native-fields' ),
-					),
+							'revisions'       => __( 'Revisions', 'acf-native-fields' ),
+							'comments'        => __( 'Comments', 'acf-native-fields' ),
+							'discussion'      => __( 'Discussion', 'acf-native-fields' ),
+							'trackbacks'      => __( 'Trackbacks', 'acf-native-fields' ),
+						),
 
-					'WooCommerce' => array(
-						// TODO: Check if installed
-						'all'                       => __( 'All', 'acf-native-fields' ),
-						'product_categories'        => __( 'Product Categories', 'acf-native-fields' ),
-						'product_tags'              => __( 'Product Tags', 'acf-native-fields' ),
-						'product_image'             => __( 'Product Image', 'acf-native-fields' ),
-						'product_gallery'           => __( 'Product Gallery', 'acf-native-fields' ),
-						'product_data'              => __( 'Product Data', 'acf-native-fields' ),
-						'product_short_description' => __( 'Product Short Description', 'acf-native-fields' ),
-						'reviews'                   => __( 'Reviews', 'acf-native-fields' ),
-					),
+						'WooCommerce' => $is_active( 'woocommerce/woocommerce.php' ) ? array(
+							'all'                       => __( 'All', 'acf-native-fields' ),
+							'product_categories'        => __( 'Product Categories', 'acf-native-fields' ),
+							'product_tags'              => __( 'Product Tags', 'acf-native-fields' ),
+							'product_image'             => __( 'Product Image', 'acf-native-fields' ),
+							'product_gallery'           => __( 'Product Gallery', 'acf-native-fields' ),
+							'product_data'              => __( 'Product Data', 'acf-native-fields' ),
+							'product_short_description' => __( 'Product Short Description', 'acf-native-fields' ),
+							'reviews'                   => __( 'Reviews', 'acf-native-fields' ),
+						) : false,
 
-					'Plugins'     => array(
-						// TODO: Check if plugins are installed
-						'yoast_seo'      => __( 'Yoast SEO', 'acf-native-fields' ),
-						'seo_framework'  => __( 'SEO Framework', 'acf-native-fields' ),
-						'classic_editor' => __( 'Classic Editor (Editor Switcher)', 'acf-native-fields' ),
-						'laygridder'     => __( 'LayGridder', 'acf-native-fields' ),
-					),
+						'Plugins'     => array_filter(
+							array(
+								'yoast_seo'      => $is_active( 'wordpress-seo/wp-seo.php' ) ? __( 'Yoast SEO', 'acf-native-fields' ) : false,
+								'seo_framework'  => $is_active( 'autodescription/autodescription.php' ) ? __( 'SEO Framework', 'acf-native-fields' ) : false,
+								'classic_editor' => $is_active( 'classic-editor/classic-editor.php' ) ? __( 'Classic Editor (Editor Switcher)', 'acf-native-fields' ) : false,
+								'laygridder'     => $is_active( 'laygridder/laygridder.php' ) ? __( 'LayGridder', 'acf-native-fields' ) : false,
+							)
+						),
 
-					'Other'       => array(
-						'custom' => __( 'Custom', 'acf-native-fields' ),
-					),
+						'Other'       => array(
+							'custom' => __( 'Custom', 'acf-native-fields' ),
+						),
+					)
 				),
 			)
 		);
